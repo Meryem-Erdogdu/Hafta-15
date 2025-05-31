0df5511fcf83a6b2b35d1e87dcc77a833e3f38c3
Meryem <127032110+Meryem-Erdogdu@users.noreply.github.com>
2025-05-31 20:34:44 +0300

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import threading
 
-# Define a function to run in a thread
 def coder(number, number2, number3, number4):
     print(f'Coder ID: {number} , {number2} , {number3} , {number4}')
 
-# Create and start 5 threads
 threads = []
 for l in range(5):
     for k in range(5):
